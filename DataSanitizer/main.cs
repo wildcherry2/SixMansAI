a24fe467a14545559ea6d392f2215f336aa5a406
wildcherry <11280682+wildcherry2@users.noreply.github.com>
2022-08-29 13:53:07 -0400

    removed old reference project

--- DataSanitizer/main.cs
+++
@@ -1,8 +0,0 @@
-﻿class MainClass {
-    static void Main(string[] args) {
-        var chat = Directory.GetCurrentDirectory() + @"\..\..\..\..\..\RawData\rank-b\July2022.json";
-        //Console.WriteLine(Directory.Exists(chat));
-        var ds = new Database.Database(@"C:\Users\tyler\Documents\Programming\AI\SixMans\RawData\score-report\July2022.json", chat);
-        ds.BuildDatabase();
-    }
-}
