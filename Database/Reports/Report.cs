ecda60356adceb079a58303655fb5d21ea484a84
wildcherry <11280682+wildcherry2@users.noreply.github.com>
2022-09-01 22:12:22 -0400

    protobuf, fuck json

---
+++ Database/Reports/Report.cs
@@ -0,0 +1,8 @@
+﻿using Database.Database.Interfaces;
+
+namespace Database.Reports; 
+
+public interface IReport : ILogger {
+    public void GenerateReport(in string path);
+    public string ParseComponent<ComponentType>(ComponentType component) where ComponentType : IDatabaseComponent;
+}
