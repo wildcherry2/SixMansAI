3291fa4488d11627ffda0080c7a3de0dab436795
wildcherry <11280682+wildcherry2@users.noreply.github.com>
2022-09-08 00:39:03 -0400

    lr = 0.01, amsgrad either is good for now

--- AIModel/mainV2.py
+++ AIModel/mainV2.py
@@ -1,5 +1,6 @@
 from cmath import exp
 from pickle import TRUE
+from sqlite3 import Date
 from wsgiref import validate
 import tensorflow as tf
 import pandas as panda
@@ -46,8 +47,8 @@ def DoTrain(num_nodes, train_color, num_epochs = 128, num_layers = 8, initialize
     valid_label = 'N = ' + str(num_nodes) + ', L = ' + str(num_layers) + ', Validation loss' + ', O = ' + exp_label
     graph[training_label] = pyplot.plot(hist.history['loss'],train_color, label=training_label)
     graph[valid_label] = pyplot.plot(hist.history['val_loss'],train_color, label=valid_label)
-    ret += ('(' + str(num_nodes) + ',' + str(num_layers) + ') TL = ' + str(loss) + "\n")
-    ret +=('(' + str(num_nodes) + ',' + str(num_layers) + ') TA = ' + str(acc) + "\n")
+    ret += ('(' + str(num_nodes) + ',' + str(num_layers) + ') ' + exp_label + ' TL = ' + str(loss) + "\n")
+    ret +=('(' + str(num_nodes) + ',' + str(num_layers) + ') ' + exp_label + ' TA = ' + str(acc) + "\n")
     return ret
 
 def OnPick(event):
@@ -61,20 +62,23 @@ def OnPick(event):
     fig.canvas.draw()
 
 results = ""
+results += DoTrain(25,'#fff370', 100, 30, "he_uniform", tf.keras.optimizers.Adam(learning_rate=0.01, amsgrad=True), "Adam(amsgrad = true, epsilon = 1e-7)")
+results += DoTrain(25,'#fc0303', 100, 30, "he_uniform", tf.keras.optimizers.Adam(learning_rate=0.01), "Adam(epsilon = 1e-7)") 
 
-results += DoTrain(25,'#fff370', 100, 30, "he_uniform", tf.keras.optimizers.Adam())
-results += DoTrain(25,'#0000FF', 100, 30, "he_uniform", tf.keras.optimizers.Adam(amsgrad=True), "Adam(amsgrad = true)") #decent
-results += DoTrain(25,'#02c775', 100, 30, "he_uniform", tf.keras.optimizers.Adam(learning_rate=0.01), "Adam(learning_rate = 0.01)")
-results += DoTrain(25,'#8902c7', 100, 30, "he_uniform", tf.keras.optimizers.Adam(learning_rate=0.0001), "Adam(learning_rate = 0.0001)")
-results += DoTrain(25,'#fc0303', 100, 30, "he_uniform", tf.keras.optimizers.Adam(learning_rate=0.01, amsgrad=True), "Adam(amsgrad = true, learning_rate = 0.01)") #insane
-results += DoTrain(25,'#4287f5', 100, 30, "he_uniform", tf.keras.optimizers.Adam(learning_rate=0.0001, amsgrad=True), "Adam(amsgrad = true, learning_rate = 0.0001)")
-results += "\n"
-results += DoTrain(25,'#90eef5', 100, 30, "he_uniform", tf.keras.optimizers.SGD(), "SGD(default)")
-results += DoTrain(25,'#eb4034', 100, 30, "he_uniform", tf.keras.optimizers.SGD(nesterov=True), "SGD(nesterov = true)") #decent
-results += DoTrain(25,'#32a852', 100, 30, "he_uniform", tf.keras.optimizers.SGD(learning_rate=0.001), "SGD(learning_rate = 0.001)")
-results += DoTrain(25,'#f740b1', 100, 30, "he_uniform", tf.keras.optimizers.SGD(learning_rate=0.0001), "SGD(learning_rate = 0.0001)")
-results += DoTrain(25,'#116d58', 100, 30, "he_uniform", tf.keras.optimizers.SGD(learning_rate=0.001, nesterov=True), "SGD(nesterov = true, learning_rate = 0.001)")
-results += DoTrain(25,'#576764', 100, 30, "he_uniform", tf.keras.optimizers.SGD(learning_rate=0.0001, nesterov=True), "SGD(nesterov = true, learning_rate = 0.0001)")
+results += DoTrain(25,'#8902c7', 100, 30, "he_uniform", tf.keras.optimizers.Adam(learning_rate=0.01, amsgrad=True, epsilon=1e-6), "Adam(amsgrad = true, epsilon = 1e-6)")
+results += DoTrain(25,'#4287f5', 100, 30, "he_uniform", tf.keras.optimizers.Adam(learning_rate=0.01, epsilon=1e-6), "Adam(epsilon = 1e-6)")
+
+results += DoTrain(25,'#02c775', 100, 30, "he_uniform", tf.keras.optimizers.Adam(learning_rate=0.01, amsgrad=True, epsilon=1e-5), "Adam(amsgrad = true, epsilon = 1e-5)")
+results += DoTrain(25,'#0000FF', 100, 30, "he_uniform", tf.keras.optimizers.Adam(learning_rate=0.01, epsilon = 1e-5), "Adam(epsilon = 1e-5)")
+#results += "\n"
+results += DoTrain(25,'#90eef5', 100, 30, "he_uniform", tf.keras.optimizers.Adam(learning_rate=0.01, amsgrad=True, epsilon=1e-4), "Adam(amsgrad = true, epsilon = 1e-4)")
+results += DoTrain(25,'#eb4034', 100, 30, "he_uniform", tf.keras.optimizers.Adam(learning_rate=0.01, epsilon=1e-4), "Adam(epsilon = 1e-4)")
+
+results += DoTrain(25,'#32a852', 100, 30, "he_uniform", tf.keras.optimizers.Adam(learning_rate=0.01, amsgrad=True, epsilon=1e-3), "Adam(amsgrad = true, epsilon = 1e-3)")
+results += DoTrain(25,'#f740b1', 100, 30, "he_uniform", tf.keras.optimizers.Adam(learning_rate=0.01, epsilon=1e-3), "Adam(epsilon = 1e-3)")
+
+results += DoTrain(25,'#116d58', 100, 30, "he_uniform", tf.keras.optimizers.Adam(learning_rate=0.01, amsgrad=True, epsilon=1e-2), "Adam(amsgrad = true, epsilon = 1e-2)")
+results += DoTrain(25,'#576764', 100, 30, "he_uniform", tf.keras.optimizers.Adam(learning_rate=0.01, epsilon=1e-2), "Adam(epsilon = 1e-2)")
 
 print(results)
 legend = pyplot.legend()
@@ -85,8 +89,13 @@ for line in lines:
     line.set_pickradius(7)
 
 
-outdir = "Results\\" + datetime.datetime.now;
-#pyplot.savefig("")
+outdir = "Results\\"
+outfile = "Figure "
+friendly_time = datetime.datetime.now().strftime("%m-%d-%Y, %H-%M-%S")
+pyplot.savefig(outdir + outfile + friendly_time + ".png")
+res_str_save = open(outdir + "Test Results " + friendly_time + ".txt", 'w')
+res_str_save.write(results)
+res_str_save.close()
 pyplot.connect('pick_event', OnPick)
 pyplot.show()
 
